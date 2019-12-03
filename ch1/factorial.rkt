363ea24af96770705eaff80d75d236b7c6e2bd60
Sam Gorinsky <sgorinsky@gmail.com>
2019-12-02 23:16:26 -0600

    lang scheme

--- ch1/factorial.rkt
+++ ch1/factorial.rkt
@@ -1,6 +1,5 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname factorial) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#lang scheme
+
 ;recursive factorial
 (define (factorial n)
   (if (= n 1) n
