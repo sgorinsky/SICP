b34371eb3f50ea1e37d0a50c9e288cdbb4afa29c
Sam Gorinsky <sgorinsky@gmail.com>
2020-05-03 00:02:43 -0500

    initialize empty-board + add helper procedures

--- ch2/hierarchical data & closure/queens.scm
+++ ch2/hierarchical data & closure/queens.scm
@@ -1,6 +1,40 @@
 #lang scheme
+;; helper functions
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (append l1 l2)
+  (if (null? l1) l2
+      (cons (car l1) (append (cdr l1) l2))))
+
+(define (filter predicate? lst)
+  (cond ((null? lst) lst)
+        ((predicate? (car lst))
+            (cons (car lst) (filter predicate? (cdr lst))))
+        (else (filter predicate? (cdr lst)))))
+
+(define (map p sequence)
+  (accumulate (lambda args (cons (p (car args)) (cadr args))) null sequence))
+
+(define (flatmap proc seq)
+  (accumulate append null (map proc seq)))
+
 ;; 2.42: complete all unfinished procs in queens proc
+(define (create-board size)
+  (define (generate-list-lists c n)
+    (define (generate-list a)
+      (if (= a 0) null
+          (cons 0 (generate-list (- a 1)))))
+    (if (= n 0) null
+        (cons (generate-list c) (generate-list-lists c (- n 1)))))
+  (generate-list-lists size size))
+         
 (define (queens board-size)
+  (define empty-board
+    (create-board board-size)) 
   (define (queen-cols k)
     (if (= k 0)
         (list empty-board)
