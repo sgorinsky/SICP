db36413b582456278481cc62c11c34b308992698
Sam Gorinsky <sgorinsky@gmail.com>
2020-05-09 23:36:03 -0500

    enumerate-interval

--- ch2/hierarchical data & closure/queens.scm
+++ ch2/hierarchical data & closure/queens.scm
@@ -1,4 +1,5 @@
 #lang scheme
+
 ;; helper functions
 (define (accumulate op initial sequence)
   (if (null? sequence)
@@ -22,7 +23,13 @@
 (define (flatmap proc seq)
   (accumulate append null (map proc seq)))
 
+(define (enumerate-interval start end)
+  (cond ((> start end) null)
+        ((= start end) (list end))
+        (else (cons start (enumerate-interval (+ start 1) end)))))
+
 ;; 2.42: complete all unfinished procs in queens proc
+
 (define (create-board size)
   (define (generate-list-lists c n)
     (define (generate-list a)
@@ -36,15 +43,13 @@
   (define empty-board
     (create-board board-size)) 
   (define (queen-cols k)
-    (if (= k 0)
-        (list empty-board)
+    (if (= k 0) (list empty-board)
         (filter
          (lambda (positions) (safe? k positions))
          (flatmap
           (lambda (rest-of-queens)
             (map (lambda (new-row)
-                   (adjoin-position
-                    new-row k rest-of-queens))
+                   (adjoin-position new-row k rest-of-queens))
                  (enumerate-interval 1 board-size)))
           (queen-cols (- k 1))))))
   (queen-cols board-size))
