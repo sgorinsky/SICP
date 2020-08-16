5aca6c2ad61951134fa75a3d1de78ded44357568
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-15 23:19:29 -0400

    using transform-painter -> flip-horiz, rotate-180, rotate-270

--- ch2/hierarchical data & closure/picture-lang.scm
+++ ch2/hierarchical data & closure/picture-lang.scm
@@ -227,3 +227,26 @@
       (lambda (frame)
         (paint-left frame)
         (paint-right frame)))))
+
+;; 2.50: flip-horiz, rotate-180, rotate-270
+(define (flip-horiz painter)
+  (transform-painter
+   painter
+   (make-vect 1.0 0.0)
+   (make-vect 0.0 0.0)
+   (make-vect 1.0 1.0)))
+
+;; only rotating frame 180 deg
+(define (rotate-180 painter)
+  (transform-painter
+   painter
+   (make-vect 1.0 1.0)
+   (make-vect 1.0 0.0)
+   (make-vect 0.0 1.0)))
+
+(define (rotate-270 painter)
+  (transform-painter
+   painter
+   (make-vect 0.0 1.0)
+   (make-vect 0.0 0.0)
+   (make-vect 1.0 1.0)))
