ef2525c2b41c67bb77ae3ef3c2d45fc60977a8ea
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-16 22:16:41 -0400

    quotation expressions ex

--- ch2/symbolic data/quotation.scm
+++ ch2/symbolic data/quotation.scm
@@ -6,6 +6,15 @@
         (else (memq x (cdr l)))))
 (memq 'apple '(x (apple sauce) y apple pear))
 
+;; 2.53: evaluating following expressions
+(list 'a 'b 'c) ;; (a b c)
+(list (list 'george)) ;; ((george))
+(cdr '((x1 x2) (y1 y2))) ;; ((y1 y1))
+(cadr '((x1 x2) (y1 y2))) ;; (y1 y1)
+(pair? (car '(a short list))) ;; #f
+(memq 'red '((red shoes) (blue socks))) ;; #f
+(memq 'red '(red shoes blue socks)) ;; (red shoes blue socks)
+
 ;; 2.54: recursive eq?
 (define (append l1 l2)
   (if (null? l1) l2
