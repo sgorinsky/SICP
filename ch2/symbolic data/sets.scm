1453407ba19c478c80cc75faf661c94ef3e8594c
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-20 20:10:56 -0400

    element-of-set

--- ch2/symbolic data/sets.scm
+++ ch2/symbolic data/sets.scm
@@ -1,2 +1,7 @@
 #lang scheme
 (require "quotation.scm" "differentiation.scm")
+
+(define (element-of-set x set)
+  (cond ((null? set) #f)
+        ((eq? (car set) x) #t)
+        (else (element-of-set x (cdr set)))))
