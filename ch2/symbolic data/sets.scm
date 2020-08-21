f96ac89400d69f7e4b33d8e1355cdaa434f08c50
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-20 20:16:02 -0400

    adjoin element, intersection-set

--- ch2/symbolic data/sets.scm
+++ ch2/symbolic data/sets.scm
@@ -5,3 +5,13 @@
   (cond ((null? set) #f)
         ((eq? (car set) x) #t)
         (else (element-of-set x (cdr set)))))
+
+(define (adjoin-element x set)
+  (if (element-of-set x set)
+      set
+      (cons x set)))
+
+(define (intersection-set a b)
+  (cond ((or (null? a) (null? b)) null)
+        ((element-of-set (car a) b) (cons (car a) (intersection-set (cdr a) b)))
+        (else (intersection-set (cdr a) b))))
