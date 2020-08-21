dc1f84dd2df8bd04cf741f607de59d84e4b4ee56
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-20 20:25:16 -0400

    union-set (w unordered list)

--- ch2/symbolic data/sets.scm
+++ ch2/symbolic data/sets.scm
@@ -1,6 +1,7 @@
 #lang scheme
 (require "quotation.scm" "differentiation.scm")
 
+;; Note that these implementations of unordered lists as sets is highly inefficient
 (define (element-of-set x set)
   (cond ((null? set) #f)
         ((eq? (car set) x) #t)
@@ -15,3 +16,9 @@
   (cond ((or (null? a) (null? b)) null)
         ((element-of-set (car a) b) (cons (car a) (intersection-set (cdr a) b)))
         (else (intersection-set (cdr a) b))))
+
+;; 2.59: Union-set
+(define (union-set a b)
+  (cond ((or (null? a)) b)
+        ((element-of-set (car a) b) (union-set (cdr a) b))
+        (else (cons (car a) (union-set (cdr a) b)))))
