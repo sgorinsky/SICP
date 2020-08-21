3b56795df6e29b83cdd2d3ac0eeab2a437d5d6c8
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-20 20:31:01 -0400

    design unordered set primitives if sets are allowed duplicate elements

--- ch2/symbolic data/sets.scm
+++ ch2/symbolic data/sets.scm
@@ -22,3 +22,20 @@
   (cond ((or (null? a)) b)
         ((element-of-set (car a) b) (union-set (cdr a) b))
         (else (cons (car a) (union-set (cdr a) b)))))
+
+;; 2.60: Design all prior primitives given non-unique elements in set
+;; element-of-set is same
+
+;; adjoin element has no need for element of set checks -> O(N)
+(define (adjoin-element-dup x set)
+  (cons x set))
+
+;; intersection-set is same too since we need to check if (car a)/(car b) is in b/a
+
+;; O(N) Union-set without element-of-set checks
+;;      Basically an append procedure
+(define (union-set-dup a b)
+  (if (null? a)
+      b
+      (cons (car a) (union-set-dup (cdr a) b))))
+  
