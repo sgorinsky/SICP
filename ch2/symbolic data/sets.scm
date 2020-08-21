14e533713fe13576b606daa97cec8298493d3b77
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-20 20:54:49 -0400

    ordered-list element-of-set intersection-set

--- ch2/symbolic data/sets.scm
+++ ch2/symbolic data/sets.scm
@@ -24,18 +24,39 @@
         (else (cons (car a) (union-set (cdr a) b)))))
 
 ;; 2.60: Design all prior primitives given non-unique elements in set
-;; element-of-set is same
+;; a. element-of-set is same
 
-;; adjoin element has no need for element of set checks -> O(N)
+;; b. adjoin element has no need for element of set checks -> O(N)
 (define (adjoin-element-dup x set)
   (cons x set))
 
-;; intersection-set is same too since we need to check if (car a)/(car b) is in b/a
+;; c. intersection-set is same too since we need to check if (car a)/(car b) is in b/a
 
-;; O(N) Union-set without element-of-set checks
-;;      Basically an append procedure
+;; d. O(N) Union-set without element-of-set checks
+;;         Basically an append procedure
 (define (union-set-dup a b)
   (if (null? a)
       b
       (cons (car a) (union-set-dup (cdr a) b))))
+
+;; Suppose we have ordered sets, we know quicker when an element is in the set
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+
+(define (intersection-set-ordered set1 set2)
+  (if (or (null? set1) (null? set2)) '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1 (intersection-set (cdr set1)
+                                          (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-set set1 (cdr set2)))))))
+
   
