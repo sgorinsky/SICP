7953818e95403776b3b8d0f2b7c9d06388f44a39
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-24 21:47:00 -0400

    decoding sample message with sample tree

--- ch2/symbolic data/huffman-encoding-trees.scm
+++ ch2/symbolic data/huffman-encoding-trees.scm
@@ -38,3 +38,29 @@
               (cons (symbol-leaf next-branch)
                     (decode-1 (cdr bits) tree)) (decode-1 (cdr bits) next-branch)))))
   (decode-1 bits tree))
+
+;; Sets of symbols and associated weights
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set) (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs) '()
+      (let ((pair (car pairs)))
+        (adjoin-set
+         (make-leaf (car pair) (cadr pair))
+         (make-leaf-set (cdr pairs))))))
+
+;; 2.67: Define an encoding tree and a sample message
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree
+                    (make-leaf 'D 1)
+                    (make-leaf 'C 1)))))
+
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+(displayln (list "decoding sample-message with sample-tree:" (decode sample-message sample-tree)))
