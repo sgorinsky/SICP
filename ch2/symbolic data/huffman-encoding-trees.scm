b462a7ccf82e5d2f21f202e9924eb2aa87691037
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-24 21:33:12 -0400

    decoding alg takes stream of bits (as list), traverses tree (0 left, 1 right), appends symbol to list if leaf node is reached

--- ch2/symbolic data/huffman-encoding-trees.scm
+++ ch2/symbolic data/huffman-encoding-trees.scm
@@ -23,3 +23,18 @@
   (list left right
         (append (symbols left) (symbols right))
         (+ (weight left) (weight right))))
+
+;; Decoding algorithm
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit: CHOOSE-BRANCH" bit))))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits) '()
+        (let ((next-branch (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree)) (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
