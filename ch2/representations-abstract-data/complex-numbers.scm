df3b40c0c6eaf6062e92b8a855e93bf28f5b5be0
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-28 22:21:14 -0400

    generic selectors

--- ch2/representations-abstract-data/complex-numbers.scm
+++ ch2/representations-abstract-data/complex-numbers.scm
@@ -70,3 +70,33 @@
   (eq? (type-tag z) 'rectangular))
 
 (define (polar? z) (eq? (type-tag z) 'polar))
+
+;; Generic Selectors
+
+(define (real-part z)
+  (cond ((rectangular? z)
+         (real-part-rectangular (contents z)))
+        ((polar? z)
+         (real-part-polar (contents z)))
+        (else (error "Unknown type: REAL-PART" z))))
+
+(define (imag-part z)
+  (cond ((rectangular? z)
+         (imag-part-rectangular (contents z)))
+        ((polar? z)
+         (imag-part-polar (contents z)))
+        (else (error "Unknown type: IMAG-PART" z))))
+
+(define (magnitude z)
+  (cond ((rectangular? z)
+         (magnitude-rectangular (contents z)))
+        ((polar? z)
+         (magnitude-polar (contents z)))
+        (else (error "Unknown type: MAGNITUDE" z))))
+
+(define (angle z)
+  (cond ((rectangular? z)
+         (angle-rectangular (contents z)))
+        ((polar? z)
+         (angle-polar (contents z)))
+        (else (error "Unknown type: ANGLE" z))))
