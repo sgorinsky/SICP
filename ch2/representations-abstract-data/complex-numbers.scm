38b5d07ce45bfa876c1ed54f48f82983fc7bf71d
Sam Gorinsky <sgorinsky@gmail.com>
2020-08-28 22:10:11 -0400

    rectangular representation complex nums

--- ch2/representations-abstract-data/complex-numbers.scm
+++ ch2/representations-abstract-data/complex-numbers.scm
@@ -21,3 +21,21 @@
                                (+ (angle z1) (angle z2))))
           z1 z2)
          (/ (magnitude z1) (magnitude z2)) (- (angle z1) (angle z2))))
+
+;; Representations of complex numbers
+
+;; Rectangular 
+
+(define (real-part z) (car z))
+(define (imag-part z) (cdr z))
+
+(define (magnitude z)
+  (sqrt (+ (square (real-part z)) (square (imag-part z)))))
+
+(define (angle z)
+  (atan (imag-part z) (real-part z)))
+
+(define (make-from-real-imag x y) (cons x y))
+
+(define (make-from-mag-ang r a)
+  (cons (* r (cos a)) (* r (sin a))))
