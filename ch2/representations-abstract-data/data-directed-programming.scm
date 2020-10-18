1c25cefad745b491ebb5474b47cc54f82a097915
Sam Gorinsky <sgorinsky@gmail.com>
2020-10-17 23:21:37 -0500

    install-product-package

--- ch2/representations-abstract-data/data-directed-programming.scm
+++ ch2/representations-abstract-data/data-directed-programming.scm
@@ -127,3 +127,35 @@
   (put 'make-sum '+
        (lambda (x y) (tag (make-sum x y))))
   'done)
+
+(define (install-product-package)
+  (define (product? x) (and (pair? x) (eq? (car x) '*)))
+  (define (multiplier p) (cadr p)) ;; second item of product list
+  (define (multiplicand p) (caddr p)) ;; third item of product list
+ 
+  (define (make-sum a1 a2)
+    (cond ((=number? a1 0) a2)
+          ((=number? a2 0) a1)
+          ((and (number? a1) (number? a2)) (+ a1 a2))
+          (else (list '+ a1 a2))))
+ 
+  (define (make-product m1 m2)
+    (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+          ((=number? m1 1) m2)
+          ((=number? m2 1) m1)
+          ((and (number? m1) (number? m2)) (* m1 m2))
+          ((and (variable? m1) (variable? m2) (eq? m1 m2)) (list '** m1 2))
+          (else (list '* m1 m2))))
+
+  (define (deriv-product exp)
+    (make-sum
+     (make-product (multiplier exp)
+                   (deriv (multiplicand exp) var))
+     (make-product (deriv (multiplier exp) var)
+                   (multiplicand exp))))
+
+  (define (tag x) (attach '+ x))
+  (put 'deriv '(*) deriv-product)
+  (put 'make-product '*
+       (lambda (x y) (tag (make-product x y))))
+  'done)
