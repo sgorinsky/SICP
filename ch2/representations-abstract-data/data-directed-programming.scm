bf8b08fabd153016b7d745a0d2f961edf6a278ce
Sam Gorinsky <sgorinsky@gmail.com>
2020-10-23 20:44:52 -0500

    equ? proc in scheme-number, rational, and complex packages

--- ch2/representations-abstract-data/data-directed-programming.scm
+++ ch2/representations-abstract-data/data-directed-programming.scm
@@ -386,3 +386,30 @@
   (cond ((number? datum) datum)
         ((pair? datum) (cdr datum))
         (else (error "Bad tagged datum: CONTENTS" datum))))
+
+;; 2.79: Define a generic equality predicate equ? that tests the equality of two numbers and install it in
+;;       the arithmetic package
+
+(define (install-scheme-number-package) 
+   ;; ... 
+   (put 'equ? '(scheme-number scheme-number) =) 
+   'done) 
+  
+(define (install-rational-package) 
+  ;; ... 
+  (define (equ? x y) 
+    (= (* (numer x) (denom y)) (* (numer y) (denom x)))) 
+  ;; ... 
+  (put 'equ? '(rational rational) equ?) 
+  'done) 
+  
+(define (install-complex-package) 
+  ;; ... 
+  (define (equ? x y) 
+    (and (= (real-part x) (real-part y)) (= (imag-part x) (imag-part y)))) 
+  ;; ... 
+  (put 'equ? '(complex complex) equ?) 
+  'done) 
+  
+(define (equ? x y) (apply-generic 'equ? x y)) 
+
