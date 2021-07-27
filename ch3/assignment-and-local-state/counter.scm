4faa44401783aeec0c01932f4c3f2d41551a0cd5
Sam Gorinsky <sgorinsky@gmail.com>
2021-07-27 00:50:56 -0500

    local-state + message-passing for counter "class"

--- ch3/assignment-and-local-state/counter.scm
+++ ch3/assignment-and-local-state/counter.scm
@@ -2,13 +2,16 @@
 
 ;; create some procedure that manages some internal state of proc instantiations
 (define counter
-  (let ((start 0))
-    (lambda (add)
-      (display start)
-      (newline)
-      (set! start (+ start 1)))))
+  (let ((count 0))
+    (lambda (message)
+      (cond ((eq? 'add message) (set! count (+ count 1)))
+            ((eq? 'display message)
+                  (display count)
+                  (newline))
+            (else (error "No proc for type message"))))))
 
 (define c1 counter)
 (c1 'add)
+(c1 'display)
 (c1 'add)
-(c1 'add)
+(c1 'display)
