fbf266216f03101c3c11b24d64c9b0eea772b85e
Sam Gorinsky <sgorinsky@gmail.com>
2021-07-30 12:43:53 -0500

    proc that monitors calls to instance of obj

--- ch3/assignment-and-local-state/local-state-variables.scm
+++ ch3/assignment-and-local-state/local-state-variables.scm
@@ -16,3 +16,22 @@
 (define B (make-accumulator 5))
 (B 10) ; 15
 (B 10) ; 25
+
+;; 3.2: Create make-monitored to keep track of how many times instance of obj was called
+(define (make-monitored fn)
+  (let ((calls 0))
+    (lambda (message)
+      (if (eq? message 'how-many-calls)
+          calls
+          ((lambda (x)
+            (set! calls (+ calls 1))
+            (fn x)) message)
+          ))))
+          
+
+(define sq-mon (make-monitored (lambda (x) (* x x))))
+(sq-mon 5)
+(sq-mon 'how-many-calls)
+(sq-mon 5)
+(sq-mon 'how-many-calls)
+  
