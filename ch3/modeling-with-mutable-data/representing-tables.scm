263d9ede0bbe7fb8511a478e671d66f723c66f8a
Sam Gorinsky <sgorinsky@gmail.com>
2021-08-07 09:36:04 -0500

    same-key? compare arg for create-table constructor

--- ch3/modeling-with-mutable-data/representing-tables.scm
+++ ch3/modeling-with-mutable-data/representing-tables.scm
@@ -18,7 +18,8 @@
 
 ;; massq -- eq? key check
 (define (massq key mlst)
-  (if (null? mlst) #f
+  (if (null? mlst)
+      #f
       (let ((pair (mcar mlst)))
         (if (eq? (mcar pair) key)
             pair
@@ -81,19 +82,19 @@
 
 ;; table object
 (define (table)
-  (let ((local-table (make-table)))
+  (let ((local-table (make-table)) (lookup-proc massoc))
     (define (lookup k1 k2)
-      (let ((subtable (massoc k1 (mcdr local-table))))
+      (let ((subtable (lookup-proc k1 (mcdr local-table))))
         (if subtable
-            (let ((record (massoc k2 (mcdr subtable))))
+            (let ((record (lookup-proc k2 (mcdr subtable))))
               (if record
                   (mcdr record)
                   #f))
             #f)))
     (define (insert! k1 k2 val)
-      (let ((subtable (massoc k1 (mcdr local-table))))
+      (let ((subtable (lookup-proc k1 (mcdr local-table))))
         (if subtable
-            (let ((record (massoc k2 (mcdr subtable))))
+            (let ((record (lookup-proc k2 (mcdr subtable))))
               (if record
                   (set-mcdr! record val)
                   (let ((new-record (mcons k2 val)))
@@ -102,9 +103,18 @@
               (let ((new-subtable (mcons k1 (mlist new-record))))
                 (set-mcdr! local-table (mcons new-subtable (mcdr local-table)))))))
       'ok)
+
+    (define (set-lookup!)
+      (display "Use 'eq?' or 'equal?' ")
+      (let ((response (read)))
+        (cond ((or (eq? response 'eq?) (eq? response 'eq)) (set! lookup-proc massq))
+              ((or (eq? response 'equal?) (eq? response 'equal)) (set! lookup-proc massoc))
+              (else (error "Improper answer: " response)))))
+    
     (define (dispatch m)
       (cond ((eq? m 'lookup) lookup)
             ((eq? m 'insert) insert!)
+            ((eq? m 'set-lookup) (set-lookup!))
             ((eq? m 'print) local-table)
             (else (error "Unknown operation: TABLE" m))))
     dispatch))
@@ -113,3 +123,48 @@
 ((lt 'insert) 'a 'b 'c)
 ((lt 'insert) 'a 'c 'd)
 ((lt 'lookup) 'a 'b)
+
+;; 3.24: Include in make-table constructor an arg same-key? which tests equality of key
+;; 3.24: same-key? for eq? instead of equal? lookups
+;;       Implementation has setter that switches out eq? and equal?
+
+(define (create-table same-key?)
+  (let ((local-table (make-table)))
+
+    (define assoc ; already defined massoc and massq above so no need to copy-paste definition
+      (if (eq? same-key? eq?)
+          massq
+          massoc))
+      
+    (define (lookup k1 k2)
+      (let ((subtable (assoc k1 (mcdr local-table))))
+        (if subtable
+            (let ((record (assoc k2 (mcdr subtable))))
+              (if record
+                  (mcdr record)
+                  #f))
+            #f)))
+    
+    (define (insert! k1 k2 val)
+      (let ((subtable (assoc k1 (mcdr local-table))))
+        (if subtable
+            (let ((record (assoc k2 (mcdr subtable))))
+              (if record
+                  (set-mcdr! record val)
+                  (let ((new-record (mcons k2 val)))
+                    (set-mcdr! subtable (mcons new-record (mcdr subtable))))))
+            (let ((new-record (mcons k2 val)))
+              (let ((new-subtable (mcons k1 (mlist new-record))))
+                (set-mcdr! local-table (mcons new-subtable (mcdr local-table)))))))
+      'ok)
+    
+    (define (dispatch m)
+      (cond ((eq? m 'lookup) lookup)
+            ((eq? m 'insert) insert!)
+            ((eq? m 'print) local-table)
+            (else (error "Unknown operation: TABLE" m))))
+    dispatch))
+
+(define ls (create-table 'eq?))
+((ls 'insert) 'a 'b 'c)
+((ls 'lookup) 'a 'b)
