d2c67940ae8ea02dc8afcf0e328525c944b05b51
Sam Gorinsky <sgorinsky@gmail.com>
2021-08-13 19:52:06 -0500

    stream of no prime factors other than 2,3,5

--- ch3/streams/infinite-streams.scm
+++ ch3/streams/infinite-streams.scm
@@ -135,5 +135,4 @@
                                (stream-cdr s2)))))))))
 
 (define S (cons-stream 1 (merge (scale-stream S 2)
-                                (merge (scale-stream S 3) (scale-stream S 5)))))
-                        
+                                (merge (scale-stream S 3) (scale-stream S 5)))))                        
