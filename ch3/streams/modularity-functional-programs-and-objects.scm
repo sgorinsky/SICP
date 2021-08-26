9516531ed6804cc0bdbd2a45f0bf14674c99e8a5
Sam Gorinsky <sgorinsky@gmail.com>
2021-08-26 18:46:09 -0500

    modular stream estimate-integral (proportion of area enclosing circle) w monte-carlo approximation

--- ch3/streams/modularity-functional-programs-and-objects.scm
+++ ch3/streams/modularity-functional-programs-and-objects.scm
@@ -1,5 +1,7 @@
 #lang sicp
 
+(define square (lambda (x) (* x x)))
+
 ; stream boilerplate
 (define the-empty-stream '())
 
@@ -18,7 +20,12 @@
   (cons-stream
    (apply proc (map stream-car streams))
    (apply stream-map (cons proc (map stream-cdr streams)))))
-  
+
+(define (stream-ref S n)
+  (if (= n 0)
+      (display (stream-car S))
+      (stream-ref (stream-cdr S) (- n 1))))
+
 (define (show-stream S n)
   (if (= n 0)
       (display '...)
@@ -74,4 +81,65 @@
                       '(reset 10) '(generate) '(generate) '(generate) '(reset 40) '(generate) '(generate) '(generate))))
 
 (define random-generated-inputs (random-number-generator inputs))
-  
+
+;; 3.82: Create stream version of estimate-integral from ex 3.5
+; foundation for stream monte-carlo version of estimate-integral
+; point prims
+(define (make-point x y)
+  (list x y))
+(define (get-x point)
+  (car point))
+(define (get-y point)
+  (cadr point))
+
+; rect prims
+(define (make-rectangle p1 p2)
+  (list p1 p2))
+(define (get-bottom-left rect)
+  (car rect))
+(define (get-top-right rect)
+  (cadr rect))
+
+; circle prims + pred
+(define (make-circle x y rad)
+  (list (list x y) rad))
+(define (get-center circle)
+  (car circle))
+(define (get-radius circle)
+  (cadr circle))
+
+(define (circle-predicate x y r)
+  (let ((circle (make-circle x y r)))
+    (let ((center (get-center circle)) (radius (get-radius circle)))
+      (define (test-point point)
+        (let ((x (get-x point)) (y (get-y point)) (cx (get-x center)) (cy (get-y center)))
+          (<= (+ (square (- x cx))
+                 (square (- y cy)))
+              (square radius))))
+      test-point)))
+
+; rand-in-range for estimate-integral
+(define (random-in-range low high)
+  (let ((range (- high low)))
+    (+ low (random range))))
+
+; iterative stream basis for monte-carlo experiments
+(define (monte-carlo experiment-stream passed failed)
+  (define (next passed failed)
+    (cons-stream
+     (/ passed (+ passed failed))
+     (monte-carlo (stream-cdr experiment-stream) passed failed)))
+  (if (stream-car experiment-stream)
+      (next (+ passed 1) failed)
+      (next passed (+ failed 1))))
+
+; will create an infinite stream of experiments
+(define (estimate-integral circle-pred rect)
+  (define (random-point-in-rect rect)
+    (list (random-in-range (get-x (get-bottom-left rect)) (get-x (get-top-right rect)))
+          (random-in-range (get-y (get-bottom-left rect)) (get-y (get-top-right rect)))))
+  (define (experiments-stream)
+      (cons-stream (random-point-in-rect rect) (experiments-stream)))
+  (monte-carlo (stream-map circle-pred (experiments-stream)) 0 0))
+
+(define enclosed (estimate-integral (circle-predicate 0 0 1) (make-rectangle (make-point -2 -2) (make-point 2 2)))) 
