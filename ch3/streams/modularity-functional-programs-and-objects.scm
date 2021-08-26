90579bcd2384f7aca4ee51f714c63c328652b1ad
Sam Gorinsky <sgorinsky@gmail.com>
2021-08-26 00:11:10 -0500

    random-numbers point infinitely point to self, handle input-stream to update self

---
+++ ch3/streams/modularity-functional-programs-and-objects.scm
@@ -0,0 +1,76 @@
+#lang sicp
+
+(define (create-stream lst)
+  (if (null? lst)
+      the-empty-stream
+      (cons-stream (car lst) (create-stream (cdr lst)))))
+  
+; stream boilerplate
+(define the-empty-stream '())
+
+(define (stream-car stream)
+  (car stream))
+
+(define (stream-cdr stream)
+  (force (cdr stream)))
+
+(define (stream-map proc . streams)
+  (cons-stream
+   (apply proc (map stream-car streams))
+   (apply stream-map (cons proc (map stream-cdr streams)))))
+
+  
+(define ones
+  (cons-stream 1 ones))
+(define integers
+  (cons-stream 1 (stream-map + ones integers)))
+  
+(define (show-stream S n)
+  (if (= n 0)
+      (display '...)
+      (begin
+        (display (stream-car S))
+        (newline)
+        (show-stream (stream-cdr S) (- n 1)))))
+
+
+; rand primitives
+(define (rand-update x)
+    (let ((a 7901) (b 7907) (m 7909))
+      (modulo (+ (* a x) b) m)))
+
+(define random-init
+  ;(random 7910)
+  3679)
+
+(define random-nums
+  (cons-stream
+   random-init
+   (stream-map rand-update random-nums)))
+
+;; 3.81: Random stream generate new random number or reset sequence to a specified random value
+(define (random-number-generator input-stream)
+  (define (extract-command command-pair)
+    (car command-pair))
+
+  (define (get-seed command-pair)
+    (if (not (pair? command-pair))
+        (error "Not a command-pair")
+        (cadr command-pair)))
+  
+  (define random-numbers
+    (cons-stream
+     random-init
+     (stream-map
+      (lambda (command-pair number)
+        (cond ((eq? (extract-command command-pair) 'generate) (rand-update number))
+              ((eq? (extract-command command-pair) 'reset) (get-seed command-pair))
+              (else (error "Incorrect command"))))
+      input-stream
+      random-numbers)))
+  
+  (stream-cdr random-numbers))
+      
+(define inputs (create-stream (list '(reset 10) '(generate) '(generate) '(generate) '(reset 40) '(generate) '(generate) '(generate))))
+(define random-generated-inputs (random-number-generator inputs))
+  
