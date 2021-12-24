68572af03da00d5e5d14bfcd80d84edabecc2b52
Sam Gorinsky <sgorinsky@gmail.com>
2021-12-24 01:25:24 -0600

    all pythag triples starting from n, non-deterministic approach w try-again abstraction for amb

--- ch4/nondeterministic-computing/amb-and-search.scm
+++ ch4/nondeterministic-computing/amb-and-search.scm
@@ -14,7 +14,7 @@
 (define (an-integer-starting-from n)
 (amb n (an-integer-starting-from (+ n 1))))
 
-; 4.35: an-integer-between
+;; 4.35: an-integer-between
 (define (an-integer-between begin end)
   (if (= begin end) end
   (amb begin (an-integer-between (+ begin 1) end))))
@@ -25,3 +25,22 @@
     (let ((j (an-integer-between i high)))
       (let ((k (an-integer-between j high)))
         (require (= (+ (* i i) (* j j)) (* k k))) (list i j k)))))
+
+
+;; 4.36: Explain why simply replacing an-integer-between with an-integer-starting-from doesn't work and
+;;       reimplement a-pythagorean-triple-starting-from
+
+; Doesn't work because there is no termination for k, it checks infinite triples
+; Thus, we can increment i infinitely but ensure we have some termination conditions for j and k
+;    so we aren't checking bad pairs forever
+(define (all-pythagorean-triples-starting-from n)
+  (let ((i (an-integer-starting-from n)))
+    (let ((j (an-integer-between (+ i 1) (* i 2))))
+      (let ((k (an-integer-between (+ j 1) (* j 2))))
+        (require (= (+ (* i i) (* j j)) (* k k)))
+        (list i j k)))))
+
+; retrying to find all infinite pairs of pythag triples
+(define try-again
+  (lambda () (amb)))
+  
