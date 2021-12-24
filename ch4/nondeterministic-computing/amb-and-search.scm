8a67ee44ef72e67bc9f359694a429a675d4b6a30
Sam Gorinsky <sgorinsky@gmail.com>
2021-12-23 23:07:27 -0600

    pythag triples in amb tree

--- ch4/nondeterministic-computing/amb-and-search.scm
+++ ch4/nondeterministic-computing/amb-and-search.scm
@@ -2,6 +2,8 @@
 
 ; amb is a special form that "ambiguously" returns a value
 ; ie. (list (amb 1 2 3) (amb 'a 'b)) has 6 different possibilities
+
+; if predicate is not satisfied, search amb tree
 (define (require p)
   (if (not p) (amb)))
 
@@ -16,3 +18,10 @@
 (define (an-integer-between begin end)
   (if (= begin end) end
   (amb begin (an-integer-between (+ begin 1) end))))
+
+; appends to amb tree a bunch of pythagorean triples
+(define (a-pythagorean-triple-between low high)
+  (let ((i (an-integer-between low high)))
+    (let ((j (an-integer-between i high)))
+      (let ((k (an-integer-between j high)))
+        (require (= (+ (* i i) (* j j)) (* k k))) (list i j k)))))
