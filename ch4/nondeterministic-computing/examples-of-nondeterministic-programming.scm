52c308d6bd55acc11f601b14110175faaa036443
Sam Gorinsky <sgorinsky@gmail.com>
2021-12-25 01:18:06 -0600

    more efficient multiple-dwelling problem

--- ch4/nondeterministic-computing/examples-of-nondeterministic-programming.scm
+++ ch4/nondeterministic-computing/examples-of-nondeterministic-programming.scm
@@ -73,3 +73,36 @@
           (list 'fletcher fletcher)
           (list 'miller miller)
           (list 'smith smith))))
+
+;; 4.39: Does order of restrictions matter for runtime of application?
+
+; Yes, distinct is by far the longest proc to run since it compares each element against all others, so if all the other restrictions
+;    make it through, distinct should be the last check
+
+;; 4.40: How many assignments of people to floors are there both before and after the distinct? check? Write a more efficient
+;;       nondeterministic multiple-dwelling proc based upon possibilities that are not already ruled out by previous restrictions.
+
+; There are 5 for each of the 5 people so there are 5^5 combinations,
+;    but we only care about the ones that meet the distinct conditions
+; After the distinct? check, there are 5! possible combinations since one person cannot occupy another person's floor
+
+; I could either nest lets to include requires that prune tree or I could just include conditions in the initializations for
+;    each person's set of possibilities in amb
+(define (effictient-multiple-dwelling)
+  (let ((baker (amb 1 2 3 4))
+        (cooper (amb 2 4))
+        (fletcher (amb 2 4))
+        (miller (amb 1 2 3 4 5))
+        (smith (amb 1 2 3 4 5)))
+
+    ; conditions for multiple dwellings
+    (require (> miller cooper))
+    (require (not (= (abs (- smith fletcher)) 1)))
+    (require (distinct? (list baker cooper fletcher miller smith)))
+
+    ; result
+    (list (list 'baker baker)
+          (list 'cooper cooper)
+          (list 'fletcher fletcher)
+          (list 'miller miller)
+          (list 'smith smith))))
