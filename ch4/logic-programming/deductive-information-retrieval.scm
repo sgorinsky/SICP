fc930a9c93549341ded036bc9bac0ad0dd955e80
Sam Gorinsky <sgorinsky@gmail.com>
2021-12-29 02:30:46 -0600

    compound rule to find people who can replace other people

--- ch4/logic-programming/deductive-information-retrieval.scm
+++ ch4/logic-programming/deductive-information-retrieval.scm
@@ -35,3 +35,24 @@
 (and (supervisor ?person ?boss)
      (not (job ?boss (computer . ?rest-of-job)))
      (job ?person ?job))
+
+;; 4.57: Define a rule that p1 can replace p2 if either p1 has the same job as p2
+;;       or someone who does p1's job can also do p2's job and if p1 and p2 are not
+;;       the same person
+
+(rule (replaceable ?p1 ?p2)
+      (or (and (job ?p1 ?j1)
+               (job ?p2 ?j2)
+               (same ?j1 ?j2))
+          (and (can-do-job ?j1 (. ?j2))
+               (not (same ?p1 ?p2)))))
+       
+; a. All people who can replace Cy D Fect
+(replaceable ?p1 (Fect Cy D))
+
+; b. All people who can replace someone who is being paid more than they are,
+;    together with the two salaries
+(and (replaceable ?p1 ?p2)
+     (salary ?p1 ?a1)
+     (salary ?p2 ?a2)
+     (lisp-value > ?p2 ?p1))
