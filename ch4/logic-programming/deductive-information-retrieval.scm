481e7851f336de36e8930085c1f07422fca6b48e
Sam Gorinsky <sgorinsky@gmail.com>
2021-12-29 00:10:11 -0600

    some compound queries

--- ch4/logic-programming/deductive-information-retrieval.scm
+++ ch4/logic-programming/deductive-information-retrieval.scm
@@ -8,6 +8,7 @@
 (job (Fect Cy D) (computer programmer))
 
 ;; 4.55: Simple queries that retrieve following
+
 ; a. All people supervised by Ben Bitdiddle
 (supervisor ?who (Bitdiddle Ben))
 
@@ -16,3 +17,20 @@
 
 ; c. Names and addresses of all people who live in Slumerville
 (address ?who (Slumerville . ?address))
+
+;; 4.56: Compound Queries that retrieve the following
+
+; a. Names of all people supervised by Ben Bitdiddle, together with their addresses
+(and (supervisor ?who (Ben Bitdiddle))
+     (address ?who ?where))
+
+; b. All people whose salary is less than Ben Bitdiddle's together with their salaries
+;    and Ben Bitdiddle's
+(and (salary (Ben Bitdiddle) ?amount)
+     (salary ?person ?salary)
+     (lisp-value > ?salary ?amount))
+
+; c. All people supervised by someone who is not in the computer division, together
+;    with the person's name and job
+(and (supervisor ?person ?super)
+     (not (job ?super (computer . ?rest-of-job)))
