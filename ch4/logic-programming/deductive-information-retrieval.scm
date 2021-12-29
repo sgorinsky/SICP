94d8451453ca9c6d4c6abdae144aab342454885b
Sam Gorinsky <sgorinsky@gmail.com>
2021-12-29 10:45:46 -0600

    4.58: big-shot rule + 4.59: meetings

--- ch4/logic-programming/deductive-information-retrieval.scm
+++ ch4/logic-programming/deductive-information-retrieval.scm
@@ -40,7 +40,7 @@
 ;;       or someone who does p1's job can also do p2's job and if p1 and p2 are not
 ;;       the same person
 
-(rule (replaceable ?p1 ?p2)
+(rule (can-replace ?p1 ?p2)
       (or (and (job ?p1 ?j1)
                (job ?p2 ?j2)
                (same ?j1 ?j2))
@@ -48,11 +48,39 @@
                (not (same ?p1 ?p2)))))
        
 ; a. All people who can replace Cy D Fect
-(replaceable ?p1 (Fect Cy D))
+(can-replace ?p1 (Fect Cy D))
 
 ; b. All people who can replace someone who is being paid more than they are,
 ;    together with the two salaries
-(and (replaceable ?p1 ?p2)
+(and (can-replace ?p1 ?p2)
      (salary ?p1 ?a1)
      (salary ?p2 ?a2)
      (lisp-value > ?p2 ?p1))
+
+;; 4.58: Define a rule that a person is a "big shot" in a division if the person works in
+;;       a division but doesn't have a supervisor in that division
+
+(rule (big-shot ?person)
+      (and (supervisor ?person ?boss)
+           (job ?person (?division . ?title))
+           (not (job ?boss (?division . ?boss-role))))) ; shouldn't find match for ?division here
+
+;; 4.59: Finding meetings for people in ord
+
+; assertions Ben made to add meetings to database
+(meeting accounting (Monday 9am))
+(meeting administration (Monday 10am))
+(meeting computer (Wednesday 3pm))
+(meeting administration (Friday 1pm))
+
+; a. Query database for all meetings that occur on Firday
+(meeting ?division (Friday ?time))
+
+; b. Alyssa P. Hacker thinks it'd be more useful to ask for person-specific meetings
+;    Finish Alyssa's rule
+(rule (meeting-time ?person ?day-and-time)
+      (and (meeting ?division ?day-and-time)
+           (job ?person (?division . ?title))))
+
+; c. Using rule, find all Wednesday meetings for Alyssa P. Hacker
+(meeting-time (Hacker Alyssa P) (Wednesday . ?time))
