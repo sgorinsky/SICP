7df8cb42b0ce2b7057db228c4381ad640e98910f
Sam Gorinsky <sgorinsky@gmail.com>
2021-12-28 23:44:58 -0600

    example queries

--- ch4/logic-programming/deductive-information-retrieval.scm
+++ ch4/logic-programming/deductive-information-retrieval.scm
@@ -6,3 +6,13 @@
 ;;; Query results:
 (job (Hacker Alyssa P) (computer programmer))
 (job (Fect Cy D) (computer programmer))
+
+;; 4.55: Simple queries that retrieve following
+; a. All people supervised by Ben Bitdiddle
+(supervisor ?who (Bitdiddle Ben))
+
+; b. Names and jobs of all people in the accounting division
+(job ?who (accounting . ?accounting-job))
+
+; c. Names and addresses of all people who live in Slumerville
+(address ?who (Slumerville . ?address))
